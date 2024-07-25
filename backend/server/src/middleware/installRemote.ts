d3f166418b09cb22a64e65c1ee3bdf5ccfba1f13
Michael Salim <salim_michaelmi@yahoo.com>
2024-07-25 23:42:09 +0100

    Feat: Handle upgrade handler for vite better

--- backend/server/src/middleware/installRemote.ts
+++ backend/server/src/middleware/installRemote.ts
@@ -1,20 +1,40 @@
 import { Express, Request } from "express";
-import { Server } from "http";
+import { Server, createServer } from "http";
 import ViteExpress from "vite-express";
 
+import { getUpgradeHandlers } from "../app";
 import { serverPluginApi } from "../pluginManager";
 
 export default async function installRemote(app: Express, server: Server) {
+  const fakeHttpServer = createServer();
+
   ViteExpress.config({
     mode: process.env.NODE_ENV === "production" ? "production" : "development",
     inlineViteConfig: {
       root: `${__dirname}/../../../../apps/remote`,
       base: "/app",
       build: { outDir: "dist" },
+      server: {
+        hmr: {
+          server: fakeHttpServer,
+        },
+      },
     },
     transformer,
   });
   await ViteExpress.bind(app, server);
+
+  const upgradeHandler = fakeHttpServer.listeners("upgrade")[0] as any;
+  if (upgradeHandler) {
+    const upgradeHandlers = getUpgradeHandlers(app);
+    upgradeHandlers.push({
+      name: "Remote Hander",
+      check(req) {
+        return (req.url === "/app" || req.url?.startsWith("/app")) ?? false;
+      },
+      upgrade: upgradeHandler,
+    });
+  }
 }
 
 function transformer(html: string, _req: Request) {
