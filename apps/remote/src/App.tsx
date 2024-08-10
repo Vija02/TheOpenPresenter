205f75eb70ade4f83dc0e351c7b6244b9df960bd
Michael Salim <salim_michaelmi@yahoo.com>
2024-08-10 21:49:40 +0100

    Feat: Better scrolling on body

--- apps/remote/src/App.tsx
+++ apps/remote/src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
 import ReactJson from "react-json-view";
 import { useSnapshot } from "valtio";
 
@@ -12,12 +12,14 @@ function App() {
   return (
     <div>
       {!!data.data && (
-        <Grid gridTemplateColumns={`200px 1fr`}>
+        <Grid gridTemplateColumns={`200px 1fr`} width="100vw" height="100vh">
           <Sidebar />
           <MainBody />
         </Grid>
       )}
-      <ReactJson src={data} />
+      <Box w="100%" overflow="auto">
+        <ReactJson src={data} />
+      </Box>
     </div>
   );
 }
