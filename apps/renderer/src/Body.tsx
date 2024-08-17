090f4bcc05b86a40dab745cddc84c8b8d15c0262
Michael Salim <salim_michaelmi@yahoo.com>
2024-08-17 20:12:24 +0100

    Feat: Memoize Renderer better

--- apps/renderer/src/Body.tsx
+++ apps/renderer/src/Body.tsx
@@ -1,6 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import { Scene } from "@repo/base-plugin";
-import React from "react";
+import React, { useMemo } from "react";
 
 import { usePluginMetaData } from "./contexts/PluginMetaDataProvider";
 import { trpcClient } from "./trpc";
@@ -15,9 +15,11 @@ import {
 export const Body = () => {
   const data = useData();
 
-  const currentRenderer = data.renderer["1"];
-  const currentScene = currentRenderer?.currentScene;
-  const pluginMetaData = usePluginMetaData();
+  const currentRenderer = useMemo(() => data.renderer["1"], [data.renderer]);
+  const currentScene = useMemo(
+    () => currentRenderer?.currentScene,
+    [currentRenderer?.currentScene],
+  );
 
   if (!currentScene) {
     return;
@@ -25,40 +27,56 @@ export const Body = () => {
 
   return (
     <>
-      {Object.keys(currentRenderer.children[currentScene] ?? {}).map(
-        (pluginId) => {
-          const pluginInfo = (mainState.data[currentScene] as Scene).children[
-            pluginId
-          ];
-          const tag = pluginMetaData?.pluginMeta.registeredRendererView.find(
-            (x) => x.pluginName === pluginInfo?.plugin,
-          )?.tag;
-
-          return (
-            <Box key={pluginId}>
-              {tag ? (
-                React.createElement(tag, {
-                  yjsPluginSceneData: getYJSPluginSceneData(
-                    currentScene,
-                    pluginId,
-                  ),
-                  yjsPluginRendererData: getYJSPluginRendererData(
-                    currentScene,
-                    pluginId,
-                  ),
-                  pluginContext: { pluginId, sceneId: currentScene },
-                  setRenderCurrentScene: () => {
-                    getYJSPluginRenderer()?.set("currentScene", currentScene);
-                  },
-                  trpcClient,
-                })
-              ) : (
-                <Text>No renderer for {pluginInfo?.plugin}</Text>
-              )}
-            </Box>
-          );
-        },
+      {Object.keys(currentRenderer?.children[currentScene] ?? {}).map(
+        (pluginId) => (
+          <PluginRenderer
+            key={pluginId}
+            pluginId={pluginId}
+            currentScene={currentScene}
+          />
+        ),
       )}
     </>
   );
 };
+
+const PluginRenderer = React.memo(
+  ({ pluginId, currentScene }: { pluginId: string; currentScene: string }) => {
+    const pluginMetaData = usePluginMetaData();
+
+    const pluginInfo = useMemo(
+      () => (mainState.data[currentScene] as Scene).children[pluginId],
+      [currentScene, pluginId],
+    );
+    const tag = useMemo(
+      () =>
+        pluginMetaData?.pluginMeta.registeredRendererView.find(
+          (x) => x.pluginName === pluginInfo?.plugin,
+        )?.tag,
+      [pluginInfo?.plugin, pluginMetaData?.pluginMeta.registeredRendererView],
+    );
+
+    const TagElement = useMemo(
+      () =>
+        tag ? (
+          React.createElement(tag, {
+            yjsPluginSceneData: getYJSPluginSceneData(currentScene, pluginId),
+            yjsPluginRendererData: getYJSPluginRendererData(
+              currentScene,
+              pluginId,
+            ),
+            pluginContext: { pluginId, sceneId: currentScene },
+            setRenderCurrentScene: () => {
+              getYJSPluginRenderer()?.set("currentScene", currentScene);
+            },
+            trpcClient,
+          })
+        ) : (
+          <Text>No renderer for {pluginInfo?.plugin}</Text>
+        ),
+      [currentScene, pluginId, pluginInfo?.plugin, tag],
+    );
+
+    return <Box key={pluginId}>{TagElement}</Box>;
+  },
+);
