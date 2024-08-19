e728acaea88a55e1d19a8c9549f5e3103d43c2e0
Michael Salim <salim_michaelmi@yahoo.com>
2024-08-19 22:28:06 +0100

    Feat(MWL): Calculate viewBox synchronously & memoize

--- plugins/myworshiplist/view/MWLRenderer/MWLSongRenderView/index.tsx
+++ plugins/myworshiplist/view/MWLRenderer/MWLSongRenderView/index.tsx
@@ -1,6 +1,7 @@
-import { useLayoutEffect, useMemo, useRef, useState } from "react";
+import React, { useMemo } from "react";
 
 import { SlideStyle } from "../../../src";
+import { getSvgMeasurement } from "../../cache";
 
 type MWLSongRenderViewProps = {
   groupedData: Record<string, string[]>;
@@ -8,70 +9,56 @@ type MWLSongRenderViewProps = {
   slideStyle: Required<SlideStyle>;
 };
 
-const MWLSongRenderView = ({
-  groupedData,
-  heading,
-  slideStyle,
-}: MWLSongRenderViewProps) => {
-  const textRef = useRef<SVGTextElement>(null);
+const MWLSongRenderView = React.memo(
+  ({ groupedData, heading, slideStyle }: MWLSongRenderViewProps) => {
+    const measuredData = useMemo(
+      () =>
+        getSvgMeasurement({ slideStyle, textLines: groupedData![heading]! }),
+      [groupedData, heading, slideStyle],
+    );
 
-  const [size, setSize] = useState({ width: 0, height: 0 });
+    const viewBox = useMemo(
+      () =>
+        [
+          0,
+          0,
+          measuredData.width + slideStyle.padding,
+          measuredData.height,
+        ].join(" "),
+      [measuredData.height, measuredData.width, slideStyle.padding],
+    );
 
-  const viewBox = useMemo(
-    () => [0, 0, size.width, size.height].join(" "),
-    [size.height, size.width],
-  );
-
-  useLayoutEffect(() => {
-    setTimeout(() => {
-      const el = textRef.current?.getBBox();
-      if (el?.width === 0 || el?.height === 0) {
-        return;
-      }
-      const state = size;
-      const width = (el?.width ?? 0) + slideStyle.padding;
-      const height = el?.height ?? 0;
-      if (state.width !== width || state.height !== height) {
-        setSize({
-          width: width,
-          height: height,
-        });
-      }
-    }, 0);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [heading, slideStyle.padding, textRef]);
-
-  return (
-    <svg
-      viewBox={viewBox}
-      xmlns="http://www.w3.org/2000/svg"
-      style={{
-        width: "100%",
-        height: "100%",
-        backgroundColor: slideStyle.isDarkMode ? "black" : "transparent",
-        overflow: "visible",
-        userSelect: "none",
-      }}
-    >
-      <text
-        ref={textRef}
-        x="50%"
+    return (
+      <svg
+        viewBox={viewBox}
+        xmlns="http://www.w3.org/2000/svg"
         style={{
-          fontFamily: "inherit",
-          fontSize: "1rem",
-          fontWeight: slideStyle.fontWeight,
-          textAnchor: "middle",
+          width: "100%",
+          height: "100%",
+          backgroundColor: slideStyle.isDarkMode ? "black" : "transparent",
+          overflow: "visible",
+          userSelect: "none",
         }}
-        fill={slideStyle.isDarkMode ? "white" : "rgb(26, 32, 44)"}
       >
-        {groupedData?.[heading]?.map((x, i) => (
-          <tspan key={i} x="50%" dy="1em">
-            {x}
-          </tspan>
-        ))}
-      </text>
-    </svg>
-  );
-};
+        <text
+          x="50%"
+          style={{
+            fontFamily: "inherit",
+            fontSize: "1rem",
+            fontWeight: slideStyle.fontWeight,
+            textAnchor: "middle",
+          }}
+          fill={slideStyle.isDarkMode ? "white" : "rgb(26, 32, 44)"}
+        >
+          {groupedData?.[heading]?.map((x, i) => (
+            <tspan key={i} x="50%" dy="1em">
+              {x}
+            </tspan>
+          ))}
+        </text>
+      </svg>
+    );
+  },
+);
 
 export default MWLSongRenderView;
