ea0afea472a993018e63865bdc1542681982b28a
Michael Salim <salim_michaelmi@yahoo.com>
2024-08-22 01:31:57 +0100

    Fix: Server public static path

--- backend/server/src/middleware/installSharedStatic.ts
+++ backend/server/src/middleware/installSharedStatic.ts
@@ -1,5 +1,11 @@
 import { Express, static as staticMiddleware } from "express";
+import path from "path";
 
 export default (app: Express) => {
-  app.use(staticMiddleware(`${__dirname}/../../public`));
+  app.use(
+    staticMiddleware(path.resolve(__dirname, "../public"), {
+      maxAge: 31536000,
+      immutable: true,
+    }),
+  );
 };
