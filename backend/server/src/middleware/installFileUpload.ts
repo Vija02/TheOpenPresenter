439a8e7219f8ae475bd06be1b7723f70fb4c6be8
Michael Salim <salim_michaelmi@yahoo.com>
2024-08-30 23:25:10 +0100

    Feat: Initial tus endpoint implementation

--- backend/server/src/middleware/installFileUpload.ts
+++ backend/server/src/middleware/installFileUpload.ts
@@ -1,7 +1,9 @@
-import { Express, static as staticMiddleware } from "express";
+import { FileStore } from "@tus/file-store";
+import { Server } from "@tus/server";
+import express, { Express, static as staticMiddleware } from "express";
 import multer from "multer";
 import path from "path";
-import { typeidUnboxed } from "typeid-js";
+import { fromString, typeidUnboxed } from "typeid-js";
 
 const storage = multer.diskStorage({
   destination: path.resolve(`${__dirname}/../../../uploads`),
@@ -19,11 +21,43 @@ const upload = multer({
   storage,
 });
 
+// TODO: Setup caddy properly
+// https://github.com/tus/tus-node-server/tree/main/packages/server#example-use-with-nginx
+const server = new Server({
+  path: "/media/upload/tus",
+  datastore: new FileStore({
+    directory: path.resolve(`${__dirname}/../../../uploads`),
+  }),
+  respectForwardedHeaders: true,
+  onUploadCreate: (req, res, upload) => {
+    // TODO: Authentication
+    // TODO: Better validation
+    if (
+      !upload.metadata ||
+      !("id" in upload.metadata) ||
+      !("extension" in upload.metadata)
+    ) {
+      throw new Error("Invalid metadata");
+    }
+
+    // This works as validation since it will throw if invalid
+    fromString(upload.metadata.id!, "media");
+
+    return Promise.resolve(res);
+  },
+  namingFunction: (req, metadata) => {
+    return `${metadata!.id}.${metadata!.extension}`;
+  },
+});
+const tusUploadServer = express();
+tusUploadServer.all("*", server.handle.bind(server));
+
 export default (app: Express) => {
   app.use(
     `/media/data`,
     staticMiddleware(path.resolve(`${__dirname}/../../../uploads`)),
   );
+  app.use("/media/upload/tus", tusUploadServer);
   app.use(`/media/upload/formData`, upload.single("file"), (req, res, next) => {
     // Shouldn't get here since we have multer
     if (!req.file) {
