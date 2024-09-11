f3f11c9dd388c75babb980b46c20d3656439762e
Michael Salim <salim_michaelmi@yahoo.com>
2024-09-12 00:21:02 +0100

    Fix: Traverser doesn't handle null

--- packages/base-plugin/src/utils/traverser.ts
+++ packages/base-plugin/src/utils/traverser.ts
@@ -4,7 +4,7 @@ import { ObjectToTypedMap } from "../types";
 export function createTraverser<T = any>(yData: any) {
   return function traverser<Y extends (x: T) => any>(
     traverseFunction?: Y,
-    returnClosestYjsObj = false
+    returnClosestYjsObj = false,
   ): ObjectToTypedMap<ReturnType<Y>> {
     if (!traverseFunction) return yData;
 
@@ -18,8 +18,12 @@ export function createTraverser<T = any>(yData: any) {
 
         const newTarget = target.get(key);
         // If not an yjs object, return it as it is
-        if (typeof newTarget !== "object" || !("doc" in newTarget)) {
-          if(returnClosestYjsObj){
+        if (
+          !newTarget ||
+          typeof newTarget !== "object" ||
+          !("doc" in newTarget)
+        ) {
+          if (returnClosestYjsObj) {
             return { [originalValue]: target };
           }
           return { [originalValue]: newTarget };
