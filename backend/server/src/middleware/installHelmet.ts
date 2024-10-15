516457c30560571fc50b7898b87f48def2d40683
Michael Salim <salim_michaelmi@yahoo.com>
2024-10-15 11:12:49 +0100

    Chore: Allow * for media-src

--- backend/server/src/middleware/installHelmet.ts
+++ backend/server/src/middleware/installHelmet.ts
@@ -41,6 +41,7 @@ export default async function installHelmet(app: Express) {
         `'nonce-${nonce}'`,
         "blob:",
       ],
+      "media-src": ["*"],
     };
 
     const options: HelmetOptions = {
