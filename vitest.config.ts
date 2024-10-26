b34da238e938a1f6703cf63f6f95145e5e1d95fc
Michael Salim <salim_michaelmi@yahoo.com>
2024-10-26 21:55:31 +0100

    Chore: Set vitest to fully serial

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    fileParallelism: false,
     poolOptions: {
       threads: {
         singleThread: true,
