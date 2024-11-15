19ed42e72a7e200b5a30837f3d09ed3085d3063d
Michael Salim <salim_michaelmi@yahoo.com>
2024-11-15 17:48:56 +0000

    Feat: Run MWL upgrade script

---
+++ backend/db/migrations/committed/000009.sql
@@ -0,0 +1,5 @@
+--! Previous: sha1:560a00d05700fa9a7b21c7eb0c09fd1b188dac63
+--! Hash: sha1:27b3ab3df76031047dd7d9ca4d2af15ed7909a92
+
+--! split: 1-current.sql
+select graphile_worker.add_job('upgrade__v1_mwl_to_lyrics');
