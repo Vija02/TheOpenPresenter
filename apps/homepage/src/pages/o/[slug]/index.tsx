66cc5793559a0a7c259263d334d87d18208661fa
Michael Salim <salim_michaelmi@yahoo.com>
2024-11-16 09:24:27 +0000

    Feat: Show some text if projects page is empty

--- apps/homepage/src/pages/o/[slug]/index.tsx
+++ apps/homepage/src/pages/o/[slug]/index.tsx
@@ -5,6 +5,7 @@ import EditProjectModal from "@/containers/EditProjectModal";
 import { getServerSidePropsDeviceType, withDeviceType } from "@/lib/DeviceType";
 import { useOrganizationSlug } from "@/lib/permissionHooks/organization";
 import {
+  Box,
   Button,
   Flex,
   Heading,
@@ -15,13 +16,15 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import {
+  CategoryFragment,
+  ProjectFragment,
   useDeleteProjectMutation,
   useOrganizationDashboardIndexPageQuery,
 } from "@repo/graphql";
 import { globalState } from "@repo/lib";
 import { DateDisplayRelative, OverlayToggle, PopConfirm } from "@repo/ui";
 import { NextPage } from "next";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { FaPlus } from "react-icons/fa";
 import { MdCoPresent } from "react-icons/md";
 import { VscEdit, VscTrash } from "react-icons/vsc";
@@ -55,6 +58,11 @@ const OrganizationPage: NextPage = () => {
     [deleteProject],
   );
 
+  const emptyProject = useMemo(
+    () => query.data?.organizationBySlug?.projects.nodes.length === 0,
+    [query.data?.organizationBySlug?.projects.nodes.length],
+  );
+
   return (
     <SharedOrgLayout title="Dashboard" sharedOrgQuery={query}>
       <Flex px={1} alignItems="center" justifyContent="space-between" mb={3}>
@@ -81,109 +89,14 @@ const OrganizationPage: NextPage = () => {
       </Flex>
 
       <VStack alignItems="center" marginBottom={2} flexWrap="wrap" spacing={0}>
-        {query.data?.organizationBySlug?.projects.nodes.map((project, i) => (
-          <LinkBox
-            key={project.id}
-            display="flex"
-            flexDir={{ base: "column", sm: "row" }}
-            width="100%"
-            py={2}
-            px={1}
-            justifyContent="space-between"
-            _hover={{ bg: "blue.50" }}
-            borderBottom={{ base: "1px solid", sm: "none" }}
-            borderColor="gray.200"
-            role="group"
-          >
-            <Flex
-              alignItems="center"
-              gap={2}
-              justifyContent={{ base: "space-between", sm: "flex-start" }}
-            >
-              <LinkOverlay href={`/app/${slug}/${project.slug}`}>
-                <Text fontWeight={{ base: 600, sm: 500 }}>
-                  {project.name !== "" ? project.name : project.slug}
-                </Text>
-                <Text fontSize="xs" color="gray.500">
-                  {project.category?.name}
-                </Text>
-              </LinkOverlay>
-              <Flex>
-                <Link href={`/render/${slug}/${project.slug}`} isExternal>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    role="button"
-                    color="gray"
-                    _hover={{ bg: "blue.100", color: "blue.700" }}
-                    opacity={{ base: 1, md: 0 }}
-                    _groupHover={{ opacity: 1 }}
-                  >
-                    <MdCoPresent />
-                  </Button>
-                </Link>
-                <OverlayToggle
-                  toggler={({ onToggle }) => (
-                    <Button
-                      variant="ghost"
-                      size="sm"
-                      role="button"
-                      color="gray"
-                      _hover={{ bg: "blue.100", color: "blue.700" }}
-                      opacity={{ base: 1, md: 0 }}
-                      _groupHover={{ opacity: 1 }}
-                      onClick={onToggle}
-                    >
-                      <VscEdit />
-                    </Button>
-                  )}
-                >
-                  <EditProjectModal
-                    project={project}
-                    organizationId={query.data?.organizationBySlug?.id}
-                    categories={
-                      query.data?.organizationBySlug?.categories.nodes ?? []
-                    }
-                  />
-                </OverlayToggle>
-
-                <PopConfirm
-                  title={`Are you sure you want to delete this project? This action is not reversible.`}
-                  onConfirm={() => handleDeleteProject(project.id)}
-                  okText="Yes"
-                  cancelText="No"
-                  key="remove"
-                >
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    role="button"
-                    color="gray"
-                    _hover={{ bg: "red.50", color: "red.400" }}
-                    opacity={{ base: 1, md: 0 }}
-                    _groupHover={{ opacity: 1 }}
-                  >
-                    <VscTrash />
-                  </Button>
-                </PopConfirm>
-              </Flex>
-            </Flex>
-            <Flex
-              flexDir={{ base: "column-reverse", sm: "row" }}
-              gap={{ base: 1, sm: 4 }}
-              alignItems={{ base: "flex-start", sm: "center" }}
-            >
-              <Flex gap={1}>
-                {project.projectTags.nodes.map((projectTag, i) => (
-                  <Tag key={i} tag={projectTag.tag!} />
-                ))}
-              </Flex>
-              <Text color="gray.700" fontSize="xs" textAlign="right">
-                Updated{" "}
-                <DateDisplayRelative date={new Date(project.updatedAt)} />
-              </Text>
-            </Flex>
-          </LinkBox>
+        {emptyProject && <EmptyProject />}
+        {query.data?.organizationBySlug?.projects.nodes.map((project) => (
+          <ProjectCard
+            project={project}
+            organizationId={query.data?.organizationBySlug?.id}
+            categories={query.data?.organizationBySlug?.categories.nodes ?? []}
+            handleDeleteProject={handleDeleteProject}
+          />
         ))}
       </VStack>
 
@@ -201,6 +114,134 @@ const OrganizationPage: NextPage = () => {
         />
       </OverlayToggle>
     </SharedOrgLayout>
+  );
+};
+
+const EmptyProject = () => {
+  return (
+    <Box mt={3} w="100%">
+      <Text fontSize="lg" fontWeight="bold">
+        Welcome to your projects page!
+      </Text>
+      <Text>
+        There's currently nothing here. Create a new project to get started.
+      </Text>
+    </Box>
+  );
+};
+
+const ProjectCard = ({
+  project,
+  organizationId,
+  categories,
+  handleDeleteProject,
+}: {
+  project: ProjectFragment;
+  organizationId: string;
+  categories: CategoryFragment[];
+  handleDeleteProject: (projectId: string) => void;
+}) => {
+  const slug = useOrganizationSlug();
+  return (
+    <LinkBox
+      key={project.id}
+      display="flex"
+      flexDir={{ base: "column", sm: "row" }}
+      width="100%"
+      py={2}
+      px={1}
+      justifyContent="space-between"
+      _hover={{ bg: "blue.50" }}
+      borderBottom={{ base: "1px solid", sm: "none" }}
+      borderColor="gray.200"
+      role="group"
+    >
+      <Flex
+        alignItems="center"
+        gap={2}
+        justifyContent={{ base: "space-between", sm: "flex-start" }}
+      >
+        <LinkOverlay href={`/app/${slug}/${project.slug}`}>
+          <Text fontWeight={{ base: 600, sm: 500 }}>
+            {project.name !== "" ? project.name : project.slug}
+          </Text>
+          <Text fontSize="xs" color="gray.500">
+            {project.category?.name}
+          </Text>
+        </LinkOverlay>
+        <Flex>
+          <Link href={`/render/${slug}/${project.slug}`} isExternal>
+            <Button
+              variant="ghost"
+              size="sm"
+              role="button"
+              color="gray"
+              _hover={{ bg: "blue.100", color: "blue.700" }}
+              opacity={{ base: 1, md: 0 }}
+              _groupHover={{ opacity: 1 }}
+            >
+              <MdCoPresent />
+            </Button>
+          </Link>
+          <OverlayToggle
+            toggler={({ onToggle }) => (
+              <Button
+                variant="ghost"
+                size="sm"
+                role="button"
+                color="gray"
+                _hover={{ bg: "blue.100", color: "blue.700" }}
+                opacity={{ base: 1, md: 0 }}
+                _groupHover={{ opacity: 1 }}
+                onClick={onToggle}
+              >
+                <VscEdit />
+              </Button>
+            )}
+          >
+            <EditProjectModal
+              project={project}
+              organizationId={organizationId}
+              categories={categories}
+            />
+          </OverlayToggle>
+
+          <PopConfirm
+            title={`Are you sure you want to delete this project? This action is not reversible.`}
+            onConfirm={() => handleDeleteProject(project.id)}
+            okText="Yes"
+            cancelText="No"
+            key="remove"
+          >
+            <Button
+              variant="ghost"
+              size="sm"
+              role="button"
+              color="gray"
+              _hover={{ bg: "red.50", color: "red.400" }}
+              opacity={{ base: 1, md: 0 }}
+              _groupHover={{ opacity: 1 }}
+            >
+              <VscTrash />
+            </Button>
+          </PopConfirm>
+        </Flex>
+      </Flex>
+      <Flex
+        flexDir={{ base: "column-reverse", sm: "row" }}
+        gap={{ base: 1, sm: 4 }}
+        alignItems={{ base: "flex-start", sm: "center" }}
+      >
+        <Flex gap={1}>
+          {project.projectTags.nodes.map((projectTag, i) => (
+            <Tag key={i} tag={projectTag.tag!} />
+          ))}
+        </Flex>
+        <Text color="gray.700" fontSize="xs" textAlign="right">
+          Updated <DateDisplayRelative date={new Date(project.updatedAt)} />
+        </Text>
+      </Flex>
+    </LinkBox>
   );
 };
 
