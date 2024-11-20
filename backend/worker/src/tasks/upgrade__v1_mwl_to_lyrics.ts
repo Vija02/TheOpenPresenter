62165af0d82520fa3c6ef62abb4c104f722b8bd5
Michael Salim <salim_michaelmi@yahoo.com>
2024-11-20 12:49:54 +0000

    Chore: Better logging on upgrade

--- backend/worker/src/tasks/upgrade__v1_mwl_to_lyrics.ts
+++ backend/worker/src/tasks/upgrade__v1_mwl_to_lyrics.ts
@@ -16,95 +16,110 @@ const task: Task = async (_, { withPgClient }) => {
   );
 
   for (const project of projects) {
-    const document = project.document;
-    const ydoc = new Y.Doc();
+    try {
+      const document = project.document;
+      const ydoc = new Y.Doc();
 
-    Y.applyUpdate(ydoc, document);
+      Y.applyUpdate(ydoc, document);
 
-    const state = ydoc?.getMap() as YState;
-    const dataMap = state.get("data");
+      const state = ydoc?.getMap() as YState;
+      const dataMap = state.get("data");
 
-    const data = proxy(dataMap!.toJSON() as StateData);
-    const unbind = bind(data, dataMap as any);
+      const data = proxy(dataMap!.toJSON() as StateData);
+      const unbind = bind(data, dataMap as any);
 
-    for (const scene of Object.values(data)) {
-      if (scene.type === "scene") {
-        for (const plugin of Object.values(scene.children)) {
-          if (
-            plugin.plugin === "myworshiplist" ||
-            plugin.plugin === "lyrics-presenter"
-          ) {
-            // 1. Migrate from old type
-            if (!plugin.pluginData.songs) {
-              plugin.pluginData.songs = [];
+      for (const scene of Object.values(data)) {
+        if (scene.type === "scene") {
+          for (const plugin of Object.values(scene.children)) {
+            if (
+              plugin.plugin === "myworshiplist" ||
+              plugin.plugin === "lyrics-presenter"
+            ) {
+              // 1. Migrate from old type
+              if (!plugin.pluginData.songs) {
+                plugin.pluginData.songs = [];
 
-              try {
-                const pluginData = plugin.pluginData as any;
+                try {
+                  const pluginData = plugin.pluginData as any;
 
-                pluginData.songIds.map((songId: number) => {
-                  const songCache = pluginData.songCache.find(
-                    (cache: any) => cache.id === songId,
-                  );
+                  pluginData.songIds.map((songId: number) => {
+                    const songCache = pluginData.songCache.find(
+                      (cache: any) => cache.id === songId,
+                    );
 
-                  pluginData.songs.push({
-                    id: songId,
-                    cachedData: songCache,
-                    setting: { displayType: "sections" },
+                    pluginData.songs.push({
+                      id: songId,
+                      cachedData: songCache,
+                      setting: { displayType: "sections" },
+                    });
                   });
-                });
 
-                delete pluginData.songIds;
-                delete pluginData.songCache;
-              } catch (e) {
-                console.error("MWL: Failed to migrate to new data version");
-              }
-            }
-
-            // 2. Migrate plugin type
-            if (plugin.plugin === "myworshiplist") {
-              plugin.plugin = "lyrics-presenter";
-            }
-
-            // 3. Migrate to new generalized format
-            try {
-              for (let i = 0; i < plugin.pluginData.songs.length; i++) {
-                const previousSong = plugin.pluginData.songs[i];
-                if (!!previousSong.cachedData) {
-                  plugin.pluginData.songs[i] = {
-                    id: typeidUnboxed(),
-                    title: previousSong?.cachedData?.title,
-                    content: previousSong?.cachedData?.content,
-                    author: previousSong?.cachedData?.author,
-                    album: null,
-                    setting: previousSong.setting,
-                    _imported: true,
-                    import: {
-                      type: "myworshiplist",
-                      meta: { id: previousSong?.cachedData?.id },
-                      importedData: previousSong?.cachedData,
-                    },
-                  };
+                  delete pluginData.songIds;
+                  delete pluginData.songCache;
+                } catch (e) {
+                  console.error("MWL: Failed to migrate to new data version");
                 }
               }
-            } catch (e) {
-              console.error("Lyrics: Failed to migrate to new data version", e);
+
+              // 2. Migrate plugin type
+              try {
+                if (plugin.plugin === "myworshiplist") {
+                  plugin.plugin = "lyrics-presenter";
+                }
+              } catch (e) {
+                console.error("Failed to migrate to new data plugin name", e);
+              }
+
+              // 3. Migrate to new generalized format
+              try {
+                for (let i = 0; i < plugin.pluginData.songs.length; i++) {
+                  const previousSong = plugin.pluginData.songs[i];
+                  if (!!previousSong.cachedData) {
+                    plugin.pluginData.songs[i] = {
+                      id: typeidUnboxed(),
+                      title: previousSong?.cachedData?.title,
+                      content: previousSong?.cachedData?.content,
+                      author: previousSong?.cachedData?.author,
+                      album: null,
+                      setting: previousSong.setting,
+                      _imported: true,
+                      import: {
+                        type: "myworshiplist",
+                        meta: { id: previousSong?.cachedData?.id },
+                        importedData: previousSong?.cachedData,
+                      },
+                    };
+                  }
+                }
+              } catch (e) {
+                console.error(
+                  "Lyrics: Failed to migrate to new data version",
+                  e,
+                );
+              }
             }
           }
         }
+
+        // SAVE TO DB
+        try {
+          await withPgClient(async (pgClient) => {
+            await pgClient.query("SET session_replication_role = replica;");
+            await pgClient.query(
+              "update app_public.projects set document = $1 where id = $2",
+              [Buffer.from(Y.encodeStateAsUpdate(ydoc)), project.id],
+            );
+            return Promise.resolve();
+          });
+        } catch (e) {
+          console.error("Failed to save the migrated data to the database", e);
+        }
       }
 
-      // SAVE TO DB
-      await withPgClient(async (pgClient) => {
-        await pgClient.query("SET session_replication_role = replica;");
-        await pgClient.query(
-          "update app_public.projects set document = $1 where id = $2",
-          [Buffer.from(Y.encodeStateAsUpdate(ydoc)), project.id],
-        );
-        return Promise.resolve();
-      });
+      unbind();
+    } catch (e) {
+      console.error("Failed to migrate project id: " + project.id, e);
     }
-
-    unbind();
   }
 };
 
