366342dd1b6c98168b667e4a062291b5e34b95aa
Michael Salim <salim_michaelmi@yahoo.com>
2024-12-05 16:40:49 +0000

    Feat: Better default zoom level

--- apps/remote/src/contexts/zoomLevel.ts
+++ apps/remote/src/contexts/zoomLevel.ts
@@ -2,6 +2,6 @@ import { ZoomLevelState } from "@repo/base-plugin";
 import { createStore } from "zustand/vanilla";
 
 export const zoomLevelStore = createStore<ZoomLevelState>((set) => ({
-  zoomLevel: 0.5,
+  zoomLevel: 0.18,
   setZoomLevel: (newLevel: number) => set(() => ({ zoomLevel: newLevel })),
 }));
