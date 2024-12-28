d834061230c94feb17b49e96a2a99dadde9e3e06
Michael Salim <salim_michaelmi@yahoo.com>
2024-12-28 15:00:12 +0000

    Feat: Save incomplete parts to filesystem rather than s3

--- backend/backend-shared/src/media/s3/OurS3Store.ts
+++ backend/backend-shared/src/media/s3/OurS3Store.ts
@@ -2,6 +2,11 @@ import { MetadataValue, Options, S3Store } from "@tus/s3-store";
 import { type KvStore, TUS_RESUMABLE, Upload } from "@tus/utils";
 import debug from "debug";
 import { Express } from "express";
+import fs, { promises as fsProm } from "node:fs";
+import type { Readable } from "node:stream";
+import { promises as streamProm } from "node:stream";
+import os from "os";
+import path from "path";
 
 import { CustomKVStore } from "../customKVStore";
 
@@ -18,6 +23,9 @@ export class OurS3Store extends S3Store {
     this.configstore = new CustomKVStore<MetadataValue>(app);
   }
 
+  // ========================================================================== //
+  // ============== Save metadata to DB rather than an .info file ============= //
+  // ========================================================================== //
   protected async getMetadata(id: string): Promise<MetadataValue> {
     const metadata = (await this.configstore.get(id))!;
     return metadata;
@@ -44,5 +52,89 @@ export class OurS3Store extends S3Store {
       "tus-version": TUS_RESUMABLE,
       file: upload,
     });
+  }
+
+  // ========================================================================== //
+  // ===== Save incomplete parts in filesystem rather than directly to S3 ===== //
+  // ========================================================================== //
+
+  // Our own function to help get the part path
+  protected getIncompletePartPath(id: string) {
+    const partKey = this.partKey(id, true);
+    const filePath = path.join(os.tmpdir(), partKey);
+
+    return filePath;
+  }
+
+  protected async uploadPart(
+    metadata: MetadataValue,
+    readStream: fs.ReadStream | Readable,
+    partNumber: number,
+  ): Promise<string> {
+    const filePath = this.getIncompletePartPath(metadata.file.id);
+    // Before we upload, let's delete the incomplete parts
+    // If nothing, then we can just ignore it
+    fsProm.rm(filePath).catch(() => {
+      /* ignore */
+    });
+    return super.uploadPart(metadata, readStream, partNumber);
+  }
+
+  protected async uploadIncompletePart(
+    id: string,
+    readStream: fs.ReadStream | Readable,
+  ): Promise<string> {
+    const filePath = this.getIncompletePartPath(id);
+
+    await streamProm.pipeline(readStream, fs.createWriteStream(filePath));
+
+    return "";
+  }
+
+  protected async downloadIncompletePart(id: string) {
+    const filePath = this.getIncompletePartPath(id);
+
+    try {
+      const stat = await fsProm.lstat(filePath);
+
+      // Don't cleanup since we'll clean it up later during uploadPart
+      const createReadStream = (_options: { cleanUpOnEnd: boolean }) => {
+        const fileReader = fs.createReadStream(filePath);
+
+        return fileReader;
+      };
+
+      return {
+        size: stat.size,
+        path: filePath,
+        createReader: createReadStream,
+      };
+    } catch (e) {
+      return;
+    }
+  }
+
+  protected async getIncompletePart(id: string): Promise<Readable | undefined> {
+    // Not used, we've overridden downloadIncompletePart
+    return;
+  }
+
+  protected async getIncompletePartSize(
+    id: string,
+  ): Promise<number | undefined> {
+    const filePath = this.getIncompletePartPath(id);
+
+    try {
+      const stat = await fsProm.lstat(filePath);
+
+      return stat.size;
+    } catch (e) {
+      return undefined;
+    }
+  }
+
+  protected async deleteIncompletePart(_id: string): Promise<void> {
+    // Don't do anything since we'll delete it during uploadPart
+    return;
   }
 }
