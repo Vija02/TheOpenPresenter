4d7ff27899b2a9f16166b893be34abb10357fa13
Michael Salim <salim_michaelmi@yahoo.com>
2024-12-30 20:30:55 +0000

    Chore: Exclude loadedPlugins from test

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,14 @@ export default defineConfig({
         singleThread: true,
       },
     },
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+      // Don't double test
+      "**/loadedPlugins/**"
+    ],
   },
 });
