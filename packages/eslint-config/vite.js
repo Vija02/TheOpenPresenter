224f3cb4594576b87e7ceb763d2907c432155de3
Michael Salim <salim_michaelmi@yahoo.com>
2025-01-12 22:17:10 +0000

    Feat: Rule against number used in boolean expressions

--- packages/eslint-config/vite.js
+++ packages/eslint-config/vite.js
@@ -1,45 +1,59 @@
-const { resolve } = require("node:path")
+const { resolve } = require("node:path");
 
-const project = resolve(process.cwd(), "tsconfig.json")
+const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: [
-		"eslint:recommended",
-		"prettier",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
-		"turbo",
-	],
-	globals: {
-		React: true,
-		JSX: true,
-	},
-	env: {
-		node: true,
-		browser: true,
-		es2020: true,
-	},
-	plugins: ["only-warn", "react-refresh"],
-	settings: {
-		"import/resolver": {
-			typescript: {
-				project,
-			},
-		},
-	},
-	ignorePatterns: [
-		// Ignore dotfiles
-		".*.js",
-		"node_modules/",
-		"dist",
-		".eslintrc.cjs",
-	],
-	rules: {
-		"@typescript-eslint/no-explicit-any": 0,
-		"react-refresh/only-export-components": [
-			"warn",
-			{ allowConstantExport: true },
-		],
-	},
-}
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "turbo",
+  ],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+    browser: true,
+    es2020: true,
+  },
+  plugins: ["only-warn", "react-refresh"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist",
+    ".eslintrc.cjs",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/strict-boolean-expressions": [
+      "error",
+      {
+				// Disallow number
+        allowNumber: false,
+        allowNullableNumber: false,
+        allowString: true,
+        allowNullableObject: true,
+        allowNullableBoolean: true,
+        allowNullableString: true,
+        allowNullableEnum: true,
+        allowAny: true,
+      },
+    ],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
