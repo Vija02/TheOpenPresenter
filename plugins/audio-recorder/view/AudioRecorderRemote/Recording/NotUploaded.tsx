dfe6203a6a2fce6344d9b1608be580757b460ca4
Michael Salim <salim_michaelmi@yahoo.com>
2025-01-18 23:16:39 +0000

    Feat(p-audio): Button to reupload

--- plugins/audio-recorder/view/AudioRecorderRemote/Recording/NotUploaded.tsx
+++ plugins/audio-recorder/view/AudioRecorderRemote/Recording/NotUploaded.tsx
@@ -1,6 +1,7 @@
-import { Button, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Stack, Text } from "@chakra-ui/react";
+import { AwarenessStore } from "@repo/base-plugin/client";
 import { PopConfirm } from "@repo/ui";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { VscTrash } from "react-icons/vsc";
 
 import { Recording } from "../../../src";
@@ -22,37 +23,110 @@ export const NotUploaded = ({ recording }: { recording: Recording }) => {
 
   if (recording.streamUploadFailed) {
     return (
-      <Stack
-        direction="row"
-        border="1px solid"
-        borderColor="gray.500"
-        p={2}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Stack gap={0}>
-          <Text fontWeight="bold">
-            Something went wrong with your recording
-          </Text>
-          <Text>
-            Sorry, we are unable to do anything about this at the moment
-          </Text>
-        </Stack>
-        <PopConfirm
-          title={`Are you sure you want to remove this recording?`}
-          onConfirm={handleRemove}
-          okText="Yes"
-          cancelText="No"
-          key="remove"
-        >
-          <Button size="sm" variant="ghost" rounded="none">
-            <VscTrash />
-          </Button>
-        </PopConfirm>
-      </Stack>
+      <StreamUploadFailed recording={recording} handleRemove={handleRemove} />
     );
   }
 
+  return <StreamUploading handleRemove={handleRemove} />;
+};
+
+const StreamUploadFailed = ({
+  recording,
+  handleRemove,
+}: {
+  recording: Recording;
+  handleRemove: () => void;
+}) => {
+  const pluginApi = usePluginAPI();
+
+  const mutableSceneData = pluginApi.scene.useValtioData();
+
+  // Check awareness for whoever holds the data for this recording
+  const awareness = pluginApi.awareness.useAwarenessData();
+  const availableMedias = useMemo(
+    () =>
+      awareness
+        .map((x: AwarenessStore<Record<string, any>>) => ({
+          awarenessUserId: x.user.id,
+          media: x?.[pluginApi.pluginContext.pluginId]?.availableMedias ?? [],
+        }))
+        .filter((x) => !!x),
+    [awareness, pluginApi.pluginContext.pluginId],
+  );
+
+  const flattened = useMemo(
+    () =>
+      availableMedias
+        .map((media) =>
+          media.media.map((x: any) => ({
+            ...x,
+            awarenessUserId: media.awarenessUserId,
+          })),
+        )
+        .flat(),
+    [availableMedias],
+  );
+  const foundIndex = useMemo(
+    () => flattened.findIndex((x) => x.name === `${recording.mediaId}.mp3`),
+    [flattened, recording.mediaId],
+  );
+
+  const onUpload = useCallback(() => {
+    const index = mutableSceneData.pluginData.recordings.findIndex(
+      (x) => x.mediaId === recording.mediaId,
+    );
+    mutableSceneData.pluginData.recordings[index]!.awarenessUserToRetry =
+      flattened[foundIndex].awarenessUserId;
+  }, [
+    flattened,
+    foundIndex,
+    mutableSceneData.pluginData.recordings,
+    recording.mediaId,
+  ]);
+
+  return (
+    <Stack
+      direction="row"
+      border="1px solid"
+      borderColor="gray.500"
+      p={2}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Stack gap={0}>
+        <Text fontWeight="bold">
+          Your recording has not been successfully uploaded
+        </Text>
+        {foundIndex > -1 && (
+          <Button onClick={onUpload} mt={2} size="sm" colorScheme="primary">
+            Upload
+          </Button>
+        )}
+        {foundIndex === -1 && (
+          <Box>
+            <Text>
+              To reupload, open the device that the audio was recorded in
+            </Text>
+          </Box>
+        )}
+        <Text>{recording.awarenessUserToRetry}</Text>
+      </Stack>
+      <PopConfirm
+        title={`Are you sure you want to remove this recording?`}
+        onConfirm={handleRemove}
+        okText="Yes"
+        cancelText="No"
+        key="remove"
+      >
+        <Button size="sm" variant="ghost" rounded="none">
+          <VscTrash />
+        </Button>
+      </PopConfirm>
+    </Stack>
+  );
+};
+
+const StreamUploading = ({ handleRemove }: { handleRemove: () => void }) => {
   return (
     <Stack
       direction="row"
