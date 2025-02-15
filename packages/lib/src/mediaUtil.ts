b53ad1d1a352cf2a92823803848ae557d753941a
Michael Salim <salim_michaelmi@yahoo.com>
2025-02-15 01:29:36 +0000

    Feat: More media util, throw error when invalid media name

--- packages/lib/src/mediaUtil.ts
+++ packages/lib/src/mediaUtil.ts
@@ -2,9 +2,12 @@ import { TypeId, fromUUID, toUUID } from "typeid-js";
 
 export const extractMediaName = (mediaName: string) => {
   const splittedKey = mediaName.split(".");
-  const mediaId = splittedKey[0];
-  const extension = splittedKey[1];
-  const uuid = toUUID(mediaId as TypeId<string>);
+  if (splittedKey.length !== 2) {
+    throw new Error("Invalid Media Name");
+  }
+  const mediaId = splittedKey[0]!;
+  const extension = splittedKey[1]!;
+  const uuid = uuidFromMediaId(mediaId);
 
   return { uuid, mediaId, extension };
 };
@@ -15,4 +18,8 @@ export const mediaIdFromUUID = (uuid: string) => {
 
 export const constructMediaName = (mediaId: string, extension: string) => {
   return mediaId + "." + extension;
+};
+
+export const uuidFromMediaId = (mediaId: string) => {
+  return toUUID(mediaId as TypeId<string>);
 };
