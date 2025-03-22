b357e9fdfa3bde733a60c7492b7e8e7555997371
Michael Salim <salim_michaelmi@yahoo.com>
2025-03-22 15:53:08 +0000

    chore: test reverse

--- tauri/node-server/script/download_nodejs.mjs
+++ tauri/node-server/script/download_nodejs.mjs
@@ -101,11 +101,11 @@ async function downloadNodejs() {
       nodeFileName = await downloadWindowsBinary();
       await copyFile(
         join(tempDir, nodeFileName),
-        join(originalPath, nodeFileName),
+        join(originalPath, "node.exe"),
       );
       await copyFile(
         join(tempDir, nodeFileName),
-        join(originalPath, "node.exe"),
+        join(originalPath, nodeFileName),
       );
     } else if (platform === "linux") {
       nodeFileName = await downloadLinuxBinary();
