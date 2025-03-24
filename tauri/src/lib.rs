e4608c06bb3bf42431783931d7f384ede0937158
Michael Salim <salim_michaelmi@yahoo.com>
2025-03-24 13:49:36 +0000

    Feat: Better loading

--- tauri/src/lib.rs
+++ tauri/src/lib.rs
@@ -88,9 +88,10 @@ pub fn run() {
             // Show splashscreen until we can reach the endpoint
             let splash_window = app.get_webview_window("splashscreen").unwrap();
             let main_window = app.get_webview_window("main").unwrap();
-
+            
             tauri::async_runtime::spawn(async move {
                 if wait_for_endpoint("http://localhost:5678/o/local").await.is_ok() {
+                    main_window.eval("window.location.reload()").unwrap();
                     main_window.show().unwrap();
                     splash_window.close().unwrap();
                 }
