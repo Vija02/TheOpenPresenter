741ba63bc13ca8dadddc0fe1b3247e4015600b3c
Michael Salim <salim_michaelmi@yahoo.com>
2025-04-16 13:14:21 +0100

    Feat: Initial export opensong feature

--- plugins/lyrics-presenter/view/Remote/Remote.tsx
+++ plugins/lyrics-presenter/view/Remote/Remote.tsx
@@ -1,15 +1,69 @@
-import { Box, Button, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 import { OverlayToggle, PluginScaffold } from "@repo/ui";
+import { useCallback } from "react";
+import { PiExportLight } from "react-icons/pi";
 import { VscAdd, VscPaintcan } from "react-icons/vsc";
 
+import { removeChords } from "../../src/processLyrics";
+import { Song } from "../../src/types";
 import { usePluginAPI } from "../pluginApi";
 import RemoteAddSongModal from "./RemoteAddSongModal";
 import SongView from "./SongView";
 import StyleSettingModal from "./StyleSettingModal";
 
+const processContent = (content: string) => {
+  // We need to export the real chord, so let's just remove it for now
+  const contentWithoutChords = removeChords(content.split("\n"));
+
+  for (let i = 0; i < contentWithoutChords.length; i++) {
+    const songLine = contentWithoutChords[i] ?? "";
+
+    // Update to opensong format
+    if (songLine.startsWith("[") && songLine.endsWith("]")) {
+      // Remove white space in heading
+      contentWithoutChords[i] = songLine.replace(/ /g, "");
+    } else if (songLine === "-") {
+      contentWithoutChords[i] = songLine.replace(/-/g, "||");
+    }
+  }
+
+  return contentWithoutChords.join("\n");
+};
+
+const exportToOpenSong = (
+  song: Song,
+) => `<?xml version="1.0" encoding="UTF-8"?><song><lyrics>${processContent(song.content)}</lyrics>
+${song.import?.importedData?.original_chord ? `<key>${song.import?.importedData?.original_chord}</key>` : ``}
+<title>${song.title}</title>
+${song.author ? `<author>${song.author}</author>` : ``}
+</song>
+`;
+
+function download(data: string, filename: string, type: string) {
+  const a = document.createElement("a");
+  const file = new Blob([data], { type: type });
+
+  const url = URL.createObjectURL(file);
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(function () {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 0);
+}
+
 const Remote = () => {
   const pluginApi = usePluginAPI();
   const songs = pluginApi.scene.useData((x) => x.pluginData.songs);
+
+  const onExport = useCallback(() => {
+    songs.forEach((song) => {
+      const convertedData = exportToOpenSong(song);
+      download(convertedData, `${song.title.replace(/ /g, "_")}.xml`, "xml");
+    });
+  }, [songs]);
 
   return (
     <PluginScaffold
@@ -54,6 +108,19 @@ const Remote = () => {
           >
             <StyleSettingModal />
           </OverlayToggle>
+          <Button
+            size="xs"
+            bg="transparent"
+            color="white"
+            border="1px solid #ffffff6b"
+            _hover={{ bg: "rgba(255, 255, 255, 0.13)" }}
+            onClick={onExport}
+          >
+            <PiExportLight />{" "}
+            <Text ml={1} fontWeight="normal" fontSize="xs">
+              Export
+            </Text>
+          </Button>
         </>
       }
       body={
