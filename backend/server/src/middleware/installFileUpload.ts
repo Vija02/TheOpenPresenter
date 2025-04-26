e3447d8543b41b0a8fba9a54f82b505c453f26ca
Michael Salim <salim_michaelmi@yahoo.com>
2025-04-26 02:20:18 +0100

    Feat: Initial image processing endpoint

--- backend/server/src/middleware/installFileUpload.ts
+++ backend/server/src/middleware/installFileUpload.ts
@@ -1,10 +1,24 @@
 import { media } from "@repo/backend-shared";
+import {
+  ALLOWED_IMAGE_WIDTH,
+  constructMediaName,
+  extractMediaName,
+  mediaIdFromUUID,
+  streamToBuffer,
+  uuidFromMediaId,
+} from "@repo/lib";
+import { logger } from "@repo/observability";
 import { Server } from "@tus/server";
 import express, { Express, static as staticMiddleware } from "express";
+import fs from "fs";
 import http from "http";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import multer from "multer";
+import os from "os";
 import { Pool } from "pg";
+import sharp from "sharp";
+import { Readable } from "stream";
+import { pipeline } from "stream/promises";
 import { TypeId, fromString, toUUID, typeidUnboxed } from "typeid-js";
 
 import { getAuthPgPool, getRootPgPool } from "./installDatabasePools";
@@ -26,6 +40,168 @@ export default (app: Express) => {
       app.use(`/media/data`, apiProxy);
     }
   }
+
+  app.use(`/media/processed/:width/:mediaName`, async (req, res) => {
+    const width = parseInt(req.params.width, 10);
+
+    // Validate
+    if (!ALLOWED_IMAGE_WIDTH.includes(width)) {
+      res.status(400).send("Invalid image width");
+      return;
+    }
+
+    let mediaDetails: ReturnType<typeof extractMediaName>;
+
+    try {
+      mediaDetails = extractMediaName(req.params.mediaName);
+    } catch (e) {
+      res.status(400).send("Invalid media name");
+      return;
+    }
+
+    // TODO: extension
+    if (!["jpg", "png", "webp"].includes(mediaDetails.extension)) {
+      res.status(400).send();
+      return;
+    }
+
+    const rootPgPool = getRootPgPool(app);
+    const mediaHandler = new media[
+      process.env.STORAGE_TYPE as "file" | "s3"
+    ].mediaHandler(rootPgPool);
+
+    // If processed file exist, redirect to data URL
+    try {
+      const {
+        rows: [row],
+      } = await rootPgPool.query(
+        `SELECT processed_media_id FROM app_public.media_image_sizes
+        WHERE 
+        image_media_id = $1 AND 
+        width = $2 AND 
+        file_type = $3
+        `,
+        [mediaDetails.uuid, width, mediaDetails.extension],
+      );
+      res.redirect(
+        301,
+        `/media/data/${constructMediaName(mediaIdFromUUID(row.processed_media_id), mediaDetails.extension)}`,
+      );
+
+      return;
+    } catch (e) {}
+
+    // If doesn't exist, then we do something
+    try {
+      // Get the metadata first
+      const {
+        rows: [mediaRow],
+      } = await rootPgPool.query(
+        `select m.*, mim.width, mim.height from app_public.medias m left join app_public.media_image_metadata mim on m.id = mim.image_media_id where m.id = $1`,
+        [mediaDetails.uuid],
+      );
+
+      // If requested size is bigger than original file, skip the resize and just give the original file
+      if (mediaRow.width && width > mediaRow.width) {
+        res.redirect(301, `/media/data/${req.params.mediaName}`);
+        return;
+      }
+
+      let rawData = await mediaHandler.store.read(req.params.mediaName);
+
+      // If image metadata doesn't exist, let's process that and store it to the db
+      if (mediaRow.width === null || mediaRow.height === null) {
+        logger.warn(
+          { mediaId: mediaRow.id },
+          "/media/processed endpoint: width or height not available on image. This should only happen on old data",
+        );
+        // Store in buffer as we consume, so that we can reuse it later
+        const bufferPromise = streamToBuffer(rawData);
+        const metadata = await new Promise<sharp.Metadata>(
+          (resolve, reject) => {
+            pipeline(
+              rawData,
+              sharp().metadata((err, metadata) => {
+                if (err) reject(err);
+                resolve(metadata);
+              }),
+            );
+          },
+        );
+        const buffer = await bufferPromise;
+        // Restore the stream to be used later on
+        rawData = Readable.from(buffer);
+
+        await rootPgPool.query(
+          `INSERT INTO app_public.media_image_metadata (image_media_id, width, height)
+            VALUES ($1, $2, $3)
+          `,
+          [mediaDetails.uuid, metadata.width, metadata.height],
+        );
+
+        // Again, if requested size is bigger than original file, skip the resize and just give the original file
+        if (metadata.width && width > metadata.width) {
+          res.redirect(301, `/media/data/${req.params.mediaName}`);
+          return;
+        }
+      }
+
+      const processedMediaId = typeidUnboxed("media");
+
+      const tempFilePath = os.tmpdir() + `/${processedMediaId}`;
+      // Convert
+      await pipeline(
+        rawData,
+        sharp().rotate().resize(width).withMetadata(),
+        fs.createWriteStream(tempFilePath),
+      );
+
+      // Upload to media
+      await mediaHandler.uploadMedia({
+        file: fs.createReadStream(tempFilePath),
+        userId: mediaRow.user_id,
+        organizationId: mediaRow.organization_id,
+        isUserUploaded: false,
+        fileSize: fs.statSync(tempFilePath).size,
+        fileExtension: mediaDetails.extension,
+        mediaId: processedMediaId,
+      });
+
+      // Add the record to DB
+      await rootPgPool.query(
+        `INSERT INTO app_public.media_image_sizes (image_media_id, processed_media_id, width, file_type)
+          VALUES ($1, $2, $3, $4)
+          `,
+        [
+          mediaDetails.uuid,
+          uuidFromMediaId(processedMediaId),
+          width,
+          mediaDetails.extension,
+        ],
+      );
+
+      // Create the dependency
+      await mediaHandler.createDependency(
+        mediaDetails.uuid,
+        uuidFromMediaId(processedMediaId),
+      );
+
+      fs.rmSync(tempFilePath);
+
+      res.redirect(
+        301,
+        `/media/data/${constructMediaName(processedMediaId, mediaDetails.extension)}`,
+      );
+    } catch (error) {
+      console.error(error);
+      logger.error(
+        { error },
+        "/media/processed endpoint: width or height not available on image. This should only happen on old data",
+      );
+      res.status(500).send("Error when processing image");
+      return;
+    }
+  });
 
   // ================================== //
   // =========== Tus Upload =========== //
