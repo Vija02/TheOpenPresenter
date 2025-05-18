e75b8cfba23f0ef27c6d1386b6e6e8e63a51e0e4
Michael Salim <salim_michaelmi@yahoo.com>
2025-05-18 01:41:12 +0100

    Chore: Remnants of cypress

--- backend/config/extra.js
+++ backend/config/extra.js
@@ -30,8 +30,5 @@ process.env.SHADOW_AUTH_DATABASE_URL = process.env.SHADOW_AUTH_DATABASE_URL
 // Always overwrite test database URL
 process.env.TEST_DATABASE_URL = `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_test`;
 
-// https://docs.cypress.io/guides/guides/environment-variables.html#Option-3-CYPRESS
-process.env.CYPRESS_ROOT_URL = process.env.ROOT_URL;
-
 // This means that we may have to rebuild docker to get this updated
 process.env.NEXT_PUBLIC_ROOT_URL = process.env.ROOT_URL;
