2fe883ce021c1a52cf24a24c8bd47a84185f1049
Michael Salim <salim_michaelmi@yahoo.com>
2025-05-18 21:34:27 +0100

    Chore: update config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -44,19 +44,19 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    ...(process.env.CI
-      ? [
-          {
-            name: "webkit",
-            use: { ...devices["Desktop Safari"] },
-          },
+    // ...(process.env.CI
+    //   ? [
+    //       {
+    //         name: "webkit",
+    //         use: { ...devices["Desktop Safari"] },
+    //       },
 
-          {
-            name: "Microsoft Edge",
-            use: { ...devices["Desktop Edge"], channel: "msedge" },
-          },
-        ]
-      : []),
+    //       {
+    //         name: "Microsoft Edge",
+    //         use: { ...devices["Desktop Edge"], channel: "msedge" },
+    //       },
+    //     ]
+    //   : []),
 
     /* Test against mobile viewports. */
     // {
