1ad8e6657d004baeaa89f6290a550c544b9ddf3f
Michael Salim <salim_michaelmi@yahoo.com>
2025-07-22 00:27:49 +0100

    Chore: Remove unused

--- packages/embedded-postgres/src/index.ts
+++ packages/embedded-postgres/src/index.ts
@@ -2,8 +2,12 @@ export { EmbeddedPostgresManager } from "./EmbeddedPostgresManager.js";
 export { ExtensionManager, PgUuidv7Extension } from "./extensions/index.js";
 export { MigrationManager } from "./migration/index.js";
 export * from "./types/index.js";
-export { runCommand, runCommandWithOutput } from "./utils/command.js";
-export { getAppDataPaths, getGraphilePaths, getEmbeddedPostgresPaths } from "./utils/paths.js";
+export { runCommand } from "./utils/command.js";
+export {
+  getAppDataPaths,
+  getGraphilePaths,
+  getEmbeddedPostgresPaths,
+} from "./utils/paths.js";
 
 // Re-export the main class as default for convenience
 export { EmbeddedPostgresManager as default } from "./EmbeddedPostgresManager.js";
--- packages/embedded-postgres/src/utils/command.ts
+++ packages/embedded-postgres/src/utils/command.ts
@@ -1,10 +1,11 @@
-import { spawn, type ChildProcess } from "child_process";
+import { type ChildProcess, spawn } from "child_process";
+
 import type { CommandOptions } from "../types/index.js";
 
 export const runCommand = async (
   command: string,
   args: string[],
-  options: CommandOptions = {}
+  options: CommandOptions = {},
 ): Promise<void> => {
   return new Promise((resolve, reject) => {
     const childProcess: ChildProcess = spawn(command, args, {
@@ -18,42 +19,6 @@ export const runCommand = async (
         resolve();
       } else {
         reject(new Error(`Process exited with code ${code}`));
-      }
-    });
-
-    childProcess.on("error", (err: Error) => {
-      reject(err);
-    });
-  });
-};
-
-export const runCommandWithOutput = async (
-  command: string,
-  args: string[],
-  options: CommandOptions = {}
-): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const childProcess: ChildProcess = spawn(command, args, {
-      cwd: options.cwd,
-      env: { ...process.env, ...options.env },
-    });
-
-    let stdout = "";
-    let stderr = "";
-
-    childProcess.stdout?.on("data", (data: Buffer) => {
-      stdout += data.toString();
-    });
-
-    childProcess.stderr?.on("data", (data: Buffer) => {
-      stderr += data.toString();
-    });
-
-    childProcess.on("close", (code: number | null) => {
-      if (code === 0) {
-        resolve(stdout);
-      } else {
-        reject(new Error(`Process exited with code ${code}. stderr: ${stderr}`));
       }
     });
 
