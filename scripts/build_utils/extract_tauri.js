ed8742643f498d280bf39574d55ee2cdd06a9e64
Michael Salim <salim_michaelmi@yahoo.com>
2025-07-22 22:32:22 +0100

    Fix: Tauri deps

--- scripts/build_utils/extract_tauri.js
+++ scripts/build_utils/extract_tauri.js
@@ -1,15 +1,13 @@
-const { nodeFileTrace } = require("@vercel/nft");
 const fs = require("fs");
 
-const files = ["./packages/embedded-postgres/dist/index.js"];
+// Just copy these straight up for tauri
+const fileList = [
+  "node_modules/embedded-postgres",
+  "node_modules/@embedded-postgres",
+];
 
 (async () => {
   try {
-    const { fileList } = await nodeFileTrace(files, {
-      ignore: (prop) =>
-        !prop.startsWith("node_modules") || prop.includes("/@repo/"),
-    });
-
     fs.mkdirSync("./nft_results", { recursive: true });
     fs.writeFileSync(
       "./nft_results/tauri.json",
