5fe7f4bd1baa82c8de01e772ecd0d7292a829607
Michael Salim <salim_michaelmi@yahoo.com>
2025-07-22 23:11:17 +0100

    Feat: Should be last fix

--- scripts/build_utils/extract_tauri.js
+++ scripts/build_utils/extract_tauri.js
@@ -1,17 +1,25 @@
+const { nodeFileTrace } = require("@vercel/nft");
 const fs = require("fs");
 
+const files = ["./packages/embedded-postgres/dist/index.js"];
+
 // Just copy these straight up for tauri
-const fileList = [
+const staticFileList = [
   "node_modules/embedded-postgres",
   "node_modules/@embedded-postgres",
 ];
 
 (async () => {
   try {
+    const { fileList } = await nodeFileTrace(files, {
+      ignore: (prop) =>
+        !prop.startsWith("node_modules") || prop.includes("/@repo/"),
+    });
+
     fs.mkdirSync("./nft_results", { recursive: true });
     fs.writeFileSync(
       "./nft_results/tauri.json",
-      JSON.stringify(Array.from(fileList)),
+      JSON.stringify(staticFileList.concat(Array.from(fileList))),
     );
   } catch (e) {
     console.error(e);
