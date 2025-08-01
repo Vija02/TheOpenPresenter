64cdb41ab2275aabc5e5c52f5e34953b91eedd04
Michael Salim <salim_michaelmi@yahoo.com>
2025-08-01 22:02:34 +0100

    Chore: Remove old homepage

--- apps/homepage/src/lib/getCSRFToken.ts
+++
@@ -1,9 +0,0 @@
-export const getCSRFToken = () => {
-  const nextDataEl = document.getElementById("__NEXT_DATA__");
-  if (!nextDataEl || !nextDataEl.textContent) {
-    throw new Error("Cannot read from __NEXT_DATA__ element");
-  }
-  const data = JSON.parse(nextDataEl.textContent);
-
-  return data.query.CSRF_TOKEN;
-};
