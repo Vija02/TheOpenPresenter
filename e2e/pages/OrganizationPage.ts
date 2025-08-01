f9c3b190df239e748e467126355c7120b199f4c5
Michael Salim <salim_michaelmi@yahoo.com>
2025-08-01 17:24:11 +0100

    Feat: Import test

--- e2e/pages/OrganizationPage.ts
+++ e2e/pages/OrganizationPage.ts
@@ -2,6 +2,8 @@ import { type Locator, type Page } from "@playwright/test";
 
 export class OrganizationPage {
   readonly newProjectButton: Locator;
+  readonly importButton: Locator;
+  readonly importCloseButton: Locator;
   readonly newProjectSaveButton: Locator;
   readonly projectCards: Locator;
 
@@ -10,6 +12,11 @@ export class OrganizationPage {
       name: "New",
       exact: true,
     });
+    this.importButton = page.getByRole("button", {
+      name: "Import",
+      exact: true,
+    });
+    this.importCloseButton = page.getByLabel("Close");
     this.newProjectSaveButton = page.getByRole("button", {
       name: "Save",
     });
