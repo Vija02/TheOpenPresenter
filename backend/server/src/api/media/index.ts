55c92fad275d94fdc76a7a68ef11586d6678de56
Michael Salim <salim_michaelmi@yahoo.com>
2025-10-05 00:17:34 +0100

    Feat: AllMediaOfProjects query

--- backend/server/src/api/media/index.ts
+++ backend/server/src/api/media/index.ts
@@ -1,4 +1,5 @@
+import { allMediaOfProjects } from "./allMediaOfProjects";
 import { completeMedia } from "./completeMedia";
 import { deleteUserMedia } from "./deleteUserMedia";
 
-export default [completeMedia, deleteUserMedia];
+export default [completeMedia, deleteUserMedia, allMediaOfProjects];
