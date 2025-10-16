b6fef3a2d32e6e34a55ecb30011554b63a7e18fc
Michael Salim <salim_michaelmi@yahoo.com>
2025-10-17 00:22:13 +0100

    Feat: Cloud project id column

--- backend/db/migrations/current/100-reset.sql
+++ backend/db/migrations/current/100-reset.sql
@@ -2,7 +2,8 @@
 ALTER TABLE app_public.projects
   DROP COLUMN IF EXISTS cloud_connection_id,
   DROP COLUMN IF EXISTS cloud_last_updated,
-  DROP COLUMN IF EXISTS cloud_should_sync;
+  DROP COLUMN IF EXISTS cloud_should_sync,
+  DROP COLUMN IF EXISTS cloud_project_id;
 
 -- 200
 drop table if exists app_public.cloud_connections;
