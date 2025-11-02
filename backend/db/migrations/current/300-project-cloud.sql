3b410f19af50348c1e6a2e02fa9c87e3371f05ca
Michael Salim <salim_michaelmi@yahoo.com>
2025-11-02 23:16:48 +0000

    Feat: Commit db

--- backend/db/migrations/current/300-project-cloud.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE app_public.projects
-  ADD COLUMN cloud_connection_id uuid null,
-  ADD COLUMN cloud_last_updated timestamptz null,
-  ADD COLUMN cloud_should_sync boolean null,
-  ADD COLUMN cloud_project_id uuid null;
-
-create index on "app_public"."projects"("cloud_connection_id");
-create unique index on "app_public"."projects"("organization_id", "cloud_project_id");
