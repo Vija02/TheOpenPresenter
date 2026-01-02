6bac3a80ffeac3faa0bd37b6f8b49ff075623829
Michael Salim <salim_michaelmi@yahoo.com>
2026-01-02 18:01:01 +0000

    Feat: Make loading show reload after some time

--- packages/ui/src/Loading/LoadingFull.tsx
+++ packages/ui/src/Loading/LoadingFull.tsx
@@ -1,6 +1,8 @@
 import { cx } from "class-variance-authority";
+import { useEffect, useState } from "react";
 
 import { DeferredLoad } from "..";
+import { Link } from "../components/ui/link";
 import "./LoadingFull.css";
 
 type PropTypes = {
@@ -14,10 +16,38 @@ export function LoadingFull({
   containerClassName,
   className,
 }: PropTypes) {
+  const [showReloadPrompt, setShowReloadPrompt] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowReloadPrompt(true);
+    }, 15000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleReload = () => {
+    window.location.reload();
+  };
+
   return (
     <DeferredLoad durationMs={defer}>
       <div className={cx("center pt-[30vh]", containerClassName)}>
-        <div className={cx("ui--loading-full", className)} />
+        <div className="ui--loading-full-wrapper">
+          <div className={cx("ui--loading-full", className)} />
+          {showReloadPrompt && (
+            <Link
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                handleReload();
+              }}
+              className="ui--loading-full-reload"
+            >
+              Page not loading? Click here to reload
+            </Link>
+          )}
+        </div>
       </div>
     </DeferredLoad>
   );
