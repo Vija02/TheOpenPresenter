aeba17fa1158ec4c432058f370de7d0bd0d9ce60
Michael Salim <salim_michaelmi@yahoo.com>
2026-03-20 17:28:40 +0000

    Feat: Abstract media types to lib

--- packages/lib/src/constants.ts
+++ packages/lib/src/constants.ts
@@ -1,4 +1,5 @@
 export const ALLOWED_IMAGE_WIDTH = [320, 640, 1200, 2048, 3840];
+
 export const SUPPORTED_IMAGE_EXTENSIONS = [
   // JPEG
   ".jpg",
@@ -20,6 +21,26 @@ export const SUPPORTED_IMAGE_EXTENSIONS = [
   // TIFF
   ".tif",
   ".tiff",
+];
+
+export const BROWSER_SUPPORTED_IMAGE_EXTENSIONS = [
+  // JPEG
+  ".jpg",
+  ".jpeg",
+  ".jpe",
+  ".jif",
+  ".jfif",
+  // PNG
+  ".png",
+  // WebP
+  ".webp",
+  // AVIF
+  ".avif",
+  // GIF
+  ".gif",
+  // SVG
+  ".svg",
+  ".svgz",
 ];
 
 export const SUPPORTED_VIDEO_EXTENSIONS = [
@@ -46,6 +67,16 @@ export const SUPPORTED_VIDEO_EXTENSIONS = [
   ".3g2",
 ];
 
+export const BROWSER_SUPPORTED_VIDEO_EXTENSIONS = [
+  // MP4 (H.264/AAC)
+  ".mp4",
+  ".m4v",
+  // WebM (VP8/VP9/Opus)
+  ".webm",
+  // MOV (some browsers, mainly Safari)
+  ".mov",
+];
+
 export const SUPPORTED_AUDIO_EXTENSIONS = [
   // MP3
   ".mp3",
@@ -64,3 +95,17 @@ export const SUPPORTED_AUDIO_EXTENSIONS = [
   ".aiff",
   ".aif",
 ];
+
+export const BROWSER_SUPPORTED_AUDIO_EXTENSIONS = [
+  // MP3
+  ".mp3",
+  // WAV
+  ".wav",
+  // OGG (Vorbis/Opus)
+  ".ogg",
+  // AAC/M4A
+  ".m4a",
+  ".aac",
+];
+
+export const HLS_EXTENSIONS = [".m3u8"];
