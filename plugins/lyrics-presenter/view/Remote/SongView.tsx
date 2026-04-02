160bec9ea019d8de16ef743929834fb435fd8c3a
Michael Salim <salim_michaelmi@yahoo.com>
2026-04-02 17:14:39 +0100

    Feat: Option to reorder song

--- plugins/lyrics-presenter/view/Remote/SongView.tsx
+++ plugins/lyrics-presenter/view/Remote/SongView.tsx
@@ -1,6 +1,12 @@
 import { Button, OverlayToggle, PopConfirm, SlideGrid } from "@repo/ui";
 import React, { useCallback, useMemo } from "react";
-import { VscEdit, VscPaintcan, VscTrash } from "react-icons/vsc";
+import {
+  VscArrowDown,
+  VscArrowUp,
+  VscEdit,
+  VscPaintcan,
+  VscTrash,
+} from "react-icons/vsc";
 
 import { getMergedSlideStyle } from "../../src/slideStyle";
 import { Song } from "../../src/types";
@@ -9,7 +15,13 @@ import RemoteEditSongModal from "./RemoteEditSongModal";
 import SongStyleOverrideModal from "./SongStyleOverrideModal";
 import { SongViewSlides } from "./SongViewSlides";
 
-const SongView = React.memo(({ song }: { song: Song }) => {
+type SongViewProps = {
+  song: Song;
+  onMoveUp?: () => void;
+  onMoveDown?: () => void;
+};
+
+const SongView = React.memo(({ song, onMoveUp, onMoveDown }: SongViewProps) => {
   const pluginApi = usePluginAPI();
   const mutableSceneData = pluginApi.scene.useValtioData();
 
@@ -32,77 +44,103 @@ const SongView = React.memo(({ song }: { song: Song }) => {
       </div>
     );
   }
-  return <SongViewInner song={song} />;
-});
-
-const SongViewInner = React.memo(({ song }: { song: Song }) => {
-  const pluginApi = usePluginAPI();
-  const mutableSceneData = pluginApi.scene.useValtioData();
-  const globalStyle = pluginApi.scene.useData((x) => x.pluginData.style);
-
-  const handleRemove = useCallback(() => {
-    const pluginData = mutableSceneData.pluginData;
-
-    pluginData.songs = pluginData.songs.filter((s) => s.id !== song.id);
-  }, [mutableSceneData.pluginData, song.id]);
-
-  const slideStyle = useMemo(
-    () => getMergedSlideStyle(globalStyle, song.styleOverride),
-    [globalStyle, song.styleOverride],
-  );
-
   return (
-    <div className="pb-4">
-      <div className="flex flex-col items-start gap-2 mb-2 sm:flex-row sm:items-center">
-        <p className="text-xl mb-0 font-bold">{song.title}</p>
-        <div className="flex">
-          <OverlayToggle
-            toggler={({ onToggle }) => (
-              <Button
-                size="sm"
-                variant="ghost"
-                onClick={onToggle}
-                data-testid="ly-edit-song"
-              >
-                <VscEdit />
-              </Button>
-            )}
-          >
-            <RemoteEditSongModal song={song} />
-          </OverlayToggle>
-          <OverlayToggle
-            toggler={({ onToggle }) => (
-              <Button
-                size="sm"
-                variant="ghost"
-                onClick={onToggle}
-                data-testid="ly-style-song"
-                title="Style Override"
-              >
-                <VscPaintcan />
-              </Button>
-            )}
-          >
-            <SongStyleOverrideModal song={song} />
-          </OverlayToggle>
-          <PopConfirm
-            title={`Are you sure you want to remove this song?`}
-            onConfirm={handleRemove}
-            okText="Yes"
-            cancelText="No"
-            key="remove"
-          >
-            <Button size="sm" variant="ghost">
-              <VscTrash />
-            </Button>
-          </PopConfirm>
-        </div>
-      </div>
-      <SlideGrid pluginAPI={pluginApi}>
-        <SongViewSlides song={song} slideStyle={slideStyle} />
-      </SlideGrid>
-    </div>
+    <SongViewInner song={song} onMoveUp={onMoveUp} onMoveDown={onMoveDown} />
   );
 });
+
+const SongViewInner = React.memo(
+  ({ song, onMoveUp, onMoveDown }: SongViewProps) => {
+    const pluginApi = usePluginAPI();
+    const mutableSceneData = pluginApi.scene.useValtioData();
+    const globalStyle = pluginApi.scene.useData((x) => x.pluginData.style);
+
+    const handleRemove = useCallback(() => {
+      const pluginData = mutableSceneData.pluginData;
+
+      pluginData.songs = pluginData.songs.filter((s) => s.id !== song.id);
+    }, [mutableSceneData.pluginData, song.id]);
+
+    const slideStyle = useMemo(
+      () => getMergedSlideStyle(globalStyle, song.styleOverride),
+      [globalStyle, song.styleOverride],
+    );
+
+    return (
+      <div className="pb-4">
+        <div className="flex items-center gap-2 mb-2">
+          <div className="flex flex-col items-start gap-2 sm:flex-row sm:items-center">
+            <p className="text-xl mb-0 font-bold">{song.title}</p>
+            <div className="flex">
+              <OverlayToggle
+                toggler={({ onToggle }) => (
+                  <Button
+                    size="sm"
+                    variant="ghost"
+                    onClick={onToggle}
+                    data-testid="ly-edit-song"
+                  >
+                    <VscEdit />
+                  </Button>
+                )}
+              >
+                <RemoteEditSongModal song={song} />
+              </OverlayToggle>
+              <OverlayToggle
+                toggler={({ onToggle }) => (
+                  <Button
+                    size="sm"
+                    variant="ghost"
+                    onClick={onToggle}
+                    data-testid="ly-style-song"
+                    title="Style Override"
+                  >
+                    <VscPaintcan />
+                  </Button>
+                )}
+              >
+                <SongStyleOverrideModal song={song} />
+              </OverlayToggle>
+              <PopConfirm
+                title={`Are you sure you want to remove this song?`}
+                onConfirm={handleRemove}
+                okText="Yes"
+                cancelText="No"
+                key="remove"
+              >
+                <Button size="sm" variant="ghost">
+                  <VscTrash />
+                </Button>
+              </PopConfirm>
+            </div>
+          </div>
+          <div className="flex ml-auto">
+            <Button
+              size="xs"
+              variant="ghost"
+              onClick={onMoveUp}
+              disabled={!onMoveUp}
+              title="Move up"
+            >
+              <VscArrowUp />
+            </Button>
+            <Button
+              size="xs"
+              variant="ghost"
+              onClick={onMoveDown}
+              disabled={!onMoveDown}
+              title="Move down"
+            >
+              <VscArrowDown />
+            </Button>
+          </div>
+        </div>
+        <SlideGrid pluginAPI={pluginApi}>
+          <SongViewSlides song={song} slideStyle={slideStyle} />
+        </SlideGrid>
+      </div>
+    );
+  },
+);
 
 export default SongView;
