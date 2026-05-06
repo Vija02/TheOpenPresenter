f6b933ef0c0ef7d7153483aaf6e1df4b56392fbb
Michael Salim <salim_michaelmi@yahoo.com>
2026-05-06 14:11:50 +0100

    Feat: Screen control request stuff

--- backend/server/src/api/screen/index.ts
+++ backend/server/src/api/screen/index.ts
@@ -1,7 +1,9 @@
 import { makePluginByCombiningPlugins } from "graphile-utils";
 
+import screenControlRequestPlugin from "./controlRequest";
 import screenLoginPlugin from "./login";
 
 export default makePluginByCombiningPlugins(
   screenLoginPlugin,
+  screenControlRequestPlugin,
 );
