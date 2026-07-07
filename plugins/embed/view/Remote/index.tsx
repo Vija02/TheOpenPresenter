85bbff9c6282d69890553f265f60e3c21db20f61
ss24afk <ss24afk@herts.ac.uk>
2026-07-07 09:57:07 +0100

    Improve Embed plugin UI

--- plugins/embed/view/Remote/index.tsx
+++ plugins/embed/view/Remote/index.tsx
@@ -2,19 +2,19 @@ import { Button, Input, PluginScaffold } from "@repo/ui";
 import { useCallback, useState } from "react";
 
 import { usePluginAPI } from "../pluginApi";
-import "./index.css";
 
 const Remote = () => {
   const pluginApi = usePluginAPI();
 
   const url = pluginApi.scene.useData((x) => x.pluginData.url);
+
   const [input, setInput] = useState(url);
 
   const mutableSceneData = pluginApi.scene.useValtioData();
 
-  const onClick = useCallback(async () => {
+  const onClick = useCallback(() => {
     mutableSceneData.pluginData.url = input;
-  }, [input, mutableSceneData.pluginData]);
+  }, [input, mutableSceneData]);
 
   const onView = useCallback(() => {
     pluginApi.renderer.setRenderCurrentScene();
@@ -24,17 +24,25 @@ const Remote = () => {
     <PluginScaffold
       title="Embed"
       body={
-        <div className="stack-col items-stretch p-3 w-full">
+        <div className="stack-col items-stretch p-3 w-full gap-2">
           <p className="font-bold">Embed URL</p>
-          <Input
-            value={input ?? ""}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Enter your url..."
-          />
-          <div className="stack-row">
-            <Button onClick={onClick}>Load/Save URL</Button>
-            <Button onClick={onView}>Open in view</Button>
+
+          <div className="stack-row gap-2 items-center">
+            <Input
+              value={input ?? ""}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Enter your URL..."
+              className="flex-1 max-w-[500px]"
+            />
+
+            <Button onClick={onClick}>Load URL</Button>
           </div>
+
+          <Button onClick={onView}>Open in view</Button>
+
+          <p className="mt-2 opacity-70 text-sm">
+            {url ? `Currently loaded: ${url}` : "No URL loaded"}
+          </p>
         </div>
       }
     />
