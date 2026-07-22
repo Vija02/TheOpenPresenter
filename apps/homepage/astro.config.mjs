e9b10d39dd2d386b62531ca850868a9bb1ab5911
Michael Salim <salim_michaelmi@yahoo.com>
2026-07-22 11:27:55 +0100

    Feat: Sitemap & robots.txt

--- apps/homepage/astro.config.mjs
+++ apps/homepage/astro.config.mjs
@@ -1,8 +1,11 @@
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://theopenpresenter.com",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
     build: {
