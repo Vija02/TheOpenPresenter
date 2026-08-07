9034f28d5f4913ee071cb76fad601498ef867018
Michael Salim <salim_michaelmi@yahoo.com>
2026-08-07 17:11:26 +0100

    Chore: Missed export & add extraenv to gitignore

--- packages/layout/src/editor/index.ts
+++ packages/layout/src/editor/index.ts
@@ -4,7 +4,6 @@ export * from "./LayoutDocEditor";
 export * from "./LayoutEditor";
 export * from "./LayoutWorkbench";
 export * from "./TemplateRail";
-export * from "./ai";
 export * from "./inspector/DocumentInspector";
 export * from "./inspector/ElementInspector";
 export * from "./inspector/primitives";
